d0e6e7623a6fa03d9db2f99d82263448c307d5b5
m1kra <mieczyslaw.krawiarz@gmail.com>
2017-09-13 13:29:51 +0200

    Adam oblicza lbp.

--- Python/LBP/lbp.py
+++ Python/LBP/lbp.py
@@ -23,15 +23,32 @@ def show_lbps(X):
 
 def calc_histograms(lbps):
     histograms = np.array([itemfreq(lbp.ravel) for lbp in lbps])
-    return np.vectorize(lambda hist: hist[:, 1]/sum(hist[:, 1]))(histograms)
+    return np.array([hist[:, 1]/sum(hist[:, 1]) for hist in histograms])
+    # return np.vectorize(lambda hist: hist[:, 1]/sum(hist[:, 1]))(histograms)
 
 
-def compare_histograms():
-    # score = cv2.compareHist(np.array(x), np.array(hist), cv2.cv.CV_COMP_CHISQR)
-    pass
+def classify_histogram(hist, train_hists):
+    hist = np.array(hist, dtype=np.float32)
+    train_hists = np.array([np.array(x, dtype=np.float32) for x in train_hists])
+    scores = np.array([cv2.compareHist(hist, x, cv2.HISTCMP_CORREL) for x in train_hists])
+    print("Score to {}".format(scores))
+    index = np.argmin(scores)
+    return Ytrain[index], scores[index]
+
+
+def calc_acc(results, Ytest):
+    return np.sum(results == Ytest) / Ytest.shape[0]
+
 
 if __name__ == "__main__":
     Xtrain, Ytrain = prepare_data('train')
-    show_lbps(Xtrain)
     Xtest, Ytest = prepare_data('test')
-    show_lbps(Xtest)
+
+    train_lbps = calc_lbps(Xtrain)
+    train_histograms = calc_histograms(train_lbps)
+
+    test_lbps = calc_lbps(Xtest)
+    test_histograms = calc_histograms(test_lbps)
+
+    results = np.array([classify_histogram(hist, train_histograms)[0] for hist in test_histograms])
+    print("Accuracy to {}".format(calc_acc(results, Ytest)))
