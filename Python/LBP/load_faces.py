56f0ec9371b285a8a3824c3507ba5ea5af8d9b31
MikolajBalcerek <mpbalcerek+github@gmail.com>
2017-09-19 19:50:22 +0200

    Masowane testy dla load_faces

--- Python/LBP/load_faces.py
+++ Python/LBP/load_faces.py
@@ -21,7 +21,7 @@ def load_data(purpose, num_people=2):
     return np.array(X), np.array(Y)
 
 
-def detect_face(img):
+def detect_face(img, scaleFactorgiven, minNeighborsgiven):
     """Wykrywa twarz na obrazku img. Zwraca mniejszy obrazek, zawierający twarz."""
     # TODO CascadeClassifier i detect Multiscale ssą - nie wykrywają zawsze twarzy, sprawność na start to 9/16
     # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) - zdjęcia są szare na wejściu
@@ -64,9 +64,12 @@ def show_detected_faces(seconds=0.5):
             cv2.waitKey(1000);
 
 
-def start_count_tests():
+def count_detection_tests(scaleFactor, minNeighbors):
+
+    #liczenie wyników testów
     countDetectedAll = 0;
     countPhotosAll = 0;
+
     print("\n Rozpoczynam wykonanie testów: \n");
     for abc in ("train", "test"):
         print("Zestaw " + str(abc) + "\n");
@@ -76,7 +79,7 @@ def start_count_tests():
         print("Załadowano " + str(countPhotos) + " zdjęć \n");
 
         print("Wykrywam twarze funkcją detect_face..\n")
-        faces = np.array([detect_face(img) for img in X])
+        faces = np.array([detect_face(img, scaleFactor, minNeighbors) for img in X])
         detected = [i for i, img in enumerate(faces) if img is not None];
         countDetected = X[detected].size;
         print(str(abc) + ":  Znaleziono " + str(countDetected) + " z wszystkich " + str(countPhotos) + "\n");
@@ -85,13 +88,40 @@ def start_count_tests():
         countDetectedAll = countDetectedAll + countDetected;
 
     print("WSZYSTKIE" + ":  Znaleziono " + str(countDetectedAll) + " z wszystkich " + str(countPhotosAll) + "\n");
-
+    return [countDetectedAll, countPhotosAll];
 
 if __name__ == '__main__':
-    start_count_tests();
 
-    #show_undetected_faces();
-    #show_detected_faces();
+    #PARAMETRY DO ZMIENIANIA DO TESTÓW
+    #  tutaj można grzebać :)
+    scaleFactor_min_range = 10; #minimalny zasięg parametru scaleFactor, wartość /100
+    scaleFactor_max_range = 1000000; #maksymalny zasięg parametru scaleFactor, wartość /100
+    scaleFactor_increment = 300000; #inkrement dla testów scaleFactor, wartość/100
+    minNeighbors_min_range = 1;  # minimalny zasięc parametru minNeighbors
+    minNeighbors_max_range = 1000000; #maksymalny zasięc parametru minNeighbors
+    minNeighbors_increment = 300000; #inkrement dla testów minNeighbors
+
+    results = []; #tablica zawierająca wyniki wszystkich testów
+    #testowanie dla ręcznie dobranyc# h zmiennych
+    score = count_detection_tests(1.2, 7);
+    results.append([1.2, 7, [score]]);
+
+
+    #badanie wpływu zmiennych na rozpoznawanie twarzy
+    for scaleFactor in range (scaleFactor_min_range, scaleFactor_max_range, scaleFactor_increment):
+        for minNeighbors in range (minNeighbors_min_range, minNeighbors_max_range, minNeighbors_increment):
+            scaleFactor = scaleFactor / 100; #inkrementacja po 0.1 * inkrment, nie da się for z float increments w Python 3
+            score = count_detection_tests(scaleFactor, minNeighbors);
+            results.append([scaleFactor, minNeighbors, [score]]);
+
+
+    #wypisywanie rezultatów
+    print("Wypisywanie wyników eksperymentu: \n")
+    print("scaleFator | minNeighbors | Detected | Out of")
+    for result in results:
+        print(str(result[0]) + " | " + str(result[1]) + " | " + str(result[2]));
+#show_undetected_faces();
+#show_detected_faces();
 
 # ja = cv2.imread("szalone.jpg")
 #     cv2.imshow("przed", ja)
